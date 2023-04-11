Fix similarity matrix calculation

server/plugins/fastcompare/algo/wrappers/elicitation.py
@@ -20,7 +20,7 @@ class MultiObjectiveSamplingFromBucketsElicitationWrapper(PreferenceElicitationB
         if "rating" not in self.ratings_df:
             self.ratings_df.loc[:, "rating"] = 1
         rating_matrix = self.ratings_df.pivot(index='user', columns='item', values="rating").fillna(0).values
-        similarity_matrix = np.float32(squareform(pdist(rating_matrix.T, "cosine")))
+        similarity_matrix = 1.0 - np.float32(squareform(pdist(rating_matrix.T, "cosine")))
         self.elicitation = MultiObjectiveSamplingFromBucketsElicitation(rating_matrix, similarity_matrix, *self.args, **self.kwargs)
 
     @classmethod

server/plugins/utils/ml_data_loader.py
@@ -343,7 +343,7 @@ class MLDataLoader:
         ratings_df_i.userId = ratings_df_i.userId.map(self.user_to_user_index)
         ratings_df_i.movieId = ratings_df_i.movieId.map(self.movie_id_to_index)
         self.rating_matrix = self.ratings_df.pivot(index='userId', columns='movieId', values="rating").fillna(0).values
-        self.similarity_matrix = np.float32(squareform(pdist(self.rating_matrix.T, "cosine")))
+        self.similarity_matrix = 1.0 - np.float32(squareform(pdist(self.rating_matrix.T, "cosine")))
         
         # Maps movie index to text description
         self.movies_df["description"] = self.movies_df.title + ' ' + self.movies_df.genres

server/plugins/utils/preference_elicitation.py
@@ -182,7 +182,7 @@ def load_data_3(elicitation_movies):
 def calculate_weight_estimate(selected_movies, elicitation_movies, return_supports=False):
     if not selected_movies:
         x = np.array([1.0, 1.0, 1.0])
-        return x / x.sum()
+        return x / x.sum(), {}
 
     loader = load_ml_dataset()
 
@@ -218,23 +218,31 @@ def calculate_weight_estimate(selected_movies, elicitation_movies, return_suppor
     novelties = 1.0 - (users_viewed_item / loader.rating_matrix.shape[0])
     novelties = novelties[movie_indices].reshape((-1, 1))
 
+    # for movie_idx in movie_indices:
+    #     if movie_idx in selected_movies:
+    #         r = loader.rating_matrix[:, movie_idx]
+    #         #r = r[r > 0.0]
+    #         r = r.mean(axis=0)
+    #         #r = r.mean()
+    #         selected_relevances.append(r) # TODO try without transposition
+    #         # selected_diversities.append(distance_matrix[movie_idx][movie_indices].mean())
+    #         d = distance_matrix[movie_idx][movie_indices].sum() / (len(movie_indices) - 1)
+    #         selected_diversities.append(d[d > 0].reshape(-1, 1))
+    #         selected_novelties.append(1.0 - (loader.rating_matrix[:, movie_idx].astype(bool).sum() / loader.rating_matrix.shape[0]))
 
-    for movie_idx in movie_indices:
-        if movie_idx in selected_movies:
-            r = loader.rating_matrix[:, movie_idx]
-            #r = r[r > 0.0]
-            r = r.mean(axis=0)
-            #r = r.mean()
-            selected_relevances.append(r) # TODO try without transposition
-            # selected_diversities.append(distance_matrix[movie_idx][movie_indices].mean())
-            d = distance_matrix[movie_idx][movie_indices].sum() / (len(movie_indices) - 1)
-            selected_diversities.append(d[d > 0].reshape(-1, 1))
-            selected_novelties.append(1.0 - (loader.rating_matrix[:, movie_idx].astype(bool).sum() / loader.rating_matrix.shape[0]))
+    for movie_idx in selected_movies:
+        r = loader.rating_matrix[:, movie_idx]
+        r = r.mean(axis=0)
+        selected_relevances.append(r)
+        d = distance_matrix[movie_idx][movie_indices].sum() / (len(movie_indices) - 1)
+        selected_diversities.append(d[d > 0].reshape(-1, 1))
+        selected_novelties.append(1.0 - (loader.rating_matrix[:, movie_idx].astype(bool).sum() / loader.rating_matrix.shape[0]))
+
     
     if (not selected_relevances) or (not selected_diversities) or (not selected_novelties):
         print(f"Something weird again: {movie_indices}, {selected_movies}")
         x = np.array([1.0, 1.0, 1.0])
-        return x / x.sum()
+        return x / x.sum(), {}
 
     # Take relevance values of all items and train CDF on it
     # Calculate CDF for relevances of all selected items and take their average