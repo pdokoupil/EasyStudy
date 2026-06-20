703354fcb9c7ef7c3ba23b02feacfbcc466646cd
Marian Šámal <marian@mariansam.eu>
2026-06-20 20:42:35 +0200

    Fix default value

--- server/plugins/utils/ml_data_loader.py
+++ server/plugins/utils/ml_data_loader.py
@@ -148,7 +148,7 @@ class MLDataLoader:
         self.ratings_path = ratings_path
         self.movies_path = movies_path
         self.tags_path = tags_path
-        self.filters = filters
+        self.filters = filters or []
         self.links_path = links_path
         self.rating_matrix_path = rating_matrix_path
 
